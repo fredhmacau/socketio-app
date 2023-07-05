c9f5e981cbdb8d98beab3bb4af218fdca754e28e
Fredh Macau <80861318+fredhmacau@users.noreply.github.com>
2023-07-05 15:14:03 +0100

    Update sockets.py

--- server/sockets.py
+++ server/sockets.py
@@ -10,18 +10,24 @@ sio_app = socketio.ASGIApp(
     socketio_path='sockets'
 )
 
-
+clients_connected=dict()
 @sio_server.event
 async def connect(sid, environ, auth):
     print(f'{sid}: connected')
+    
     await sio_server.emit('join', {'sid': sid})
-
+    sio_server.enter_room(sid,sid)
 
 @sio_server.event
 async def chat(sid, message):
-    await sio_server.emit('chat', {'sid': sid, 'message': message})
+    
+    await sio_server.emit('chat', {'sid': sid, 'message': message},room=sid)
 
 
 @sio_server.event
 async def disconnect(sid):
     print(f'{sid}: disconnected')
+    #remove the session
+    sio_server.leave_room(sid, sid)
+    
+    
